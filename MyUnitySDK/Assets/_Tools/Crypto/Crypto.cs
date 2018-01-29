02ea159d0a588ce377ec823c62a744ca9d201065
yt09 <yangtao09@foxmail.com>
2018-01-29 15:17:29 +0800

    添加工具: 1.IO 写入读取文件 2.Crypto 加密解密(压缩解压缩)对象 3.Serialize 序列化与反序列化

---
+++ MyUnitySDK/Assets/_Tools/Crypto/Crypto.cs
@@ -0,0 +1,131 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace MyUnitySDK.Crypto
+{
+    /// <summary>
+    /// 加密压缩处理接口
+    /// </summary>
+    public abstract class AbsCrypto
+    {
+        /// <summary>
+        /// 加密,压缩
+        /// </summary>
+        /// <param name="content">需要加密内容</param>
+        /// <returns></returns>
+        public abstract byte[] Encrypt(byte[] content);
+
+        /// <summary>
+        /// 解密,解压缩
+        /// </summary>
+        /// <param name="content">需要解密内容</param>
+        /// <returns></returns>
+        public abstract byte[] Decrypt(byte[] content);
+    }
+
+    /// <summary>
+    /// 加解密(压缩解压缩)处理工具
+    /// </summary>
+    public class Crypto
+    {
+        public static AbsCrypto cryptoTool;
+
+        public static byte[] Encrypt(byte[] content)
+        {
+            return cryptoTool.Encrypt(content);
+        }
+
+        public static byte[] Decrypt(byte[] content)
+        {
+            return cryptoTool.Decrypt(content);
+        }
+    }
+}
+
+namespace MyUnitySDK.Crypto
+{
+    /// <summary>
+    /// 简单加解密工具
+    /// </summary>
+    public class SimpleCryptoTool : AbsCrypto
+    {
+        public override byte[] Decrypt(byte[] content)
+        {
+            for (int i = 0; i < content.Length; i++)
+            {
+                content[i] = (byte)~content[i];
+            }
+            return content;
+        }
+
+        public override byte[] Encrypt(byte[] content)
+        {
+            for (int i = 0; i < content.Length; i++)
+            {
+                content[i] = (byte)~content[i];
+            }
+            return content;
+        }
+    }
+
+    /// <summary>
+    /// GZip压缩工具(不能用于压缩大于 4 GB )
+    /// </summary>
+    public class GZipCryptoTool : AbsCrypto
+    {
+        public override byte[] Decrypt(byte[] content)
+        {
+            using (MemoryStream inputStream = new MemoryStream(content))
+            {
+                using (MemoryStream outStream = new MemoryStream())
+                {
+                    using (GZipStream zipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+                    {
+                        InternalCopyTo(zipStream, outStream, 4096);
+                        zipStream.Close();
+                        return outStream.ToArray();
+                    }
+                }
+            }
+        }
+
+        public override byte[] Encrypt(byte[] content)
+        {
+            using (MemoryStream outStream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(outStream, CompressionMode.Compress, true))
+                {
+                    zipStream.Write(content, 0, content.Length);
+                    zipStream.Close(); //很重要，必须关闭，否则无法正确解压
+                    return outStream.ToArray();
+                }
+            }
+        }
+
+        private void InternalCopyTo(Stream from, Stream destination, int bufferSize)
+        {
+            byte[] array = new byte[bufferSize];
+            int count;
+            while ((count = from.Read(array, 0, array.Length)) != 0)
+            {
+                destination.Write(array, 0, count);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 不处理
+    /// </summary>
+    public class NoCryptoTool : AbsCrypto
+    {
+        public override byte[] Decrypt(byte[] content)
+        {
+            return content;
+        }
+
+        public override byte[] Encrypt(byte[] content)
+        {
+            return content;
+        }
+    }
+}
---
+++ MyUnitySDK/Assets/_Tools/IO/IO.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+
+namespace MyUnitySDK.IO
+{
+    /// <summary>
+    /// IO接口
+    /// </summary>
+    public abstract class AbsIO
+    {
+        public abstract void Write(string config, byte[] contents);
+
+        public abstract byte[] Read(string config);
+    }
+
+    /// <summary>
+    /// IO工具
+    /// </summary>
+    public class IO
+    {
+        /// <summary>
+        /// 默认为普通text
+        /// </summary>
+        public static AbsIO ioTool;
+
+        /// <summary>
+        /// 存贮
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="content"></param>
+        /// <param name="encoding"></param>
+        public static void Write(string config, byte[] content)
+        {
+            ioTool.Write(config, content);
+        }
+
+        /// <summary>
+        /// 读取
+        /// </summary>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static byte[] Read(string config)
+        {
+            return ioTool.Read(config);
+        }
+    }
+}
+
+namespace MyUnitySDK.IO
+{
+    /// <summary>
+    /// 本地IO工具
+    /// </summary>
+    public class FileStreamIOTool : AbsIO
+    {
+        public string path;
+
+        /// <summary>
+        /// 本地io生成文件的位置
+        /// </summary>
+        /// <param name="path"></param>
+        public FileStreamIOTool(string path)
+        {
+            this.path = path;
+        }
+
+        public override byte[] Read(string config)
+        {
+            FileStream stream = new FileStream(path + "/" + config, FileMode.Open);
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, bytes.Length);
+            stream.Close();
+            return bytes;
+        }
+
+        public override void Write(string config, byte[] contents)
+        {
+            FileStream stream = new FileStream(path + "/" + config, FileMode.Create);
+            stream.Write(contents, 0, contents.Length);
+            stream.Close();
+        }
+    }
+}
